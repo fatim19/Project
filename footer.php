e247162a26602b1b1bd35195ecc93b7d9a1b68a9
FmaazDeveloper <FmaazDeveloper@gmail.com>
2023-02-07 15:38:48 +0300

    create and connection for head and footer file

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="col-1">
+        <h3>Machines Station</h3>
+       <p> @2022 Machines Station. All rights reseved</p>
+    </div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+
+</div>
---
+++ head.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/Aboutus.css">
+</head>
+<body>
+<nav class="nav"> 
+
+<div class="nav-header">
+
+<div class="nav-links">
+<ul> 
+
+<form action="connect.php" method="post">
+<a><img src="img/Machinestation.png" class="logo" href="index2.html"></a>
+<li><a href="index2.html" class="split">Machines Station</a></li>
+<li><a href="login.php">Login</a></li>
+<li><div class="dropdown">
+    <button class="dropbtn">Signup
+<i class="fa fa-caret-down"></i>
+<!-- start profile -->
+
+<!-- end profile -->
+    </button>
+    <div class="dropdown-content">
+      <a href="serviceprovidersignup.php">Signup as a service provider</a>
+      <a href="customer_register.php">Signup as a customer</a>
+    </div>
+  </li>
+  <li><a href="service2.html">Our service providers</a></li>
+<li><a href="FAQ.html">FAQ</a></li>
+
+</ul> 
+</div>
+
+
+</nav>
+</body>
+</html>
