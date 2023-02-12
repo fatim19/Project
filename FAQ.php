b9864200e67aacf4cf308ab169c8bf32c37c43b1
fatim19 <alhdryfatmh@gmail.com>
2023-02-12 12:44:34 +0300

    e

--- FAQ.php
+++ FAQ.php
@@ -3,6 +3,7 @@
 <?php
 require_once 'head.php';
 ?>
+
   <head>
 
     <meta charset="UTF-8">
@@ -18,24 +19,42 @@ require_once 'head.php';
 <div class="container">
     <h1>FAQ</h1>
     <details>
-    <summary>How can you join as a provider</summary>
+    <summary>What’s Machines Station website?</summary>
     <div>
-   The tags <em>details</em> and <em>summary</em> have you covered.
+      The Machines Station is a website that consists several of service providers that offer different kinds of services. The website helps the client find the service he/she wants easily.
     </div>
   </details>
   <details>
-    <summary>How can you order</summary>
+    <summary>How to join us as a service provider?</summary>
     <div>
-   The tags <em>details</em> and <em>summary</em> have you covered.
-    </div>
+      On the home page you choose the sign up as provider option in the home page, you are required to complete all the information requirements. Once you have registered, you can sign in and view your profile.    </div>
   </details>    
   <details>
-    <summary>How do I create accordion?</summary>
+    <summary>What kinds of service we provide?</summary>
     <div>
-   The tags <em>details</em> and <em>summary</em> have you covered.
-    </div>
+      In a Machined Station, several heavy, expensive machines are combined, including a 3D printer, laser cutter, CNC, professional camera, and vacuum cleaner, for instance.    </div>
   </details>
-  
+  <details>
+    <summary>How to make an order as a client?</summary>
+    <div>
+      When you sign up, you can view all the providers' profiles and select one of them. You can see the machines he/she is renting or the services he/she is offering.  </details>
   </div>
+  <details>
+    <summary>How to contact service provider?</summary>
+    <div>
+      Through the email address or the phone number on the provider profile, the client can contact the provider.
+    </div>
+    </details>
+    <details>
+      <summary>When can I get my order?</summary>
+      <div>
+        Providers may vary the length of time it takes to complete your order based on your order.     
+       </div>
+      </details>
+
+      <?php
+require_once 'footer.php';
+?>
+
   </body>
 </html>
